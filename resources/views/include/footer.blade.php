88778e04a8740bdc3c376787889a5a65ba0a6ee1
Himynameisfaizan <faizansaikh830@gmail.com>
2026-01-10 18:34:08 +0530

    responsive header and footer

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -4,64 +4,76 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Responsive Footer</title>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.7.0/fonts/remixicon.css" rel="stylesheet" />
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-    <div class="px-6 py-15 flex justify-around bg-[#242220] mt-5">
-        <div class="logo flex flex-col gap-6">
+    
+    <div class="bg-[#242220] mt-5 px-6 py-12 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8 justify-items-start">
+        
+        <div class="logo flex flex-col gap-6 w-full">
             <div class="img">
-               <a href="{{ url('/') }}"> <img class="w-50" src="storage/image/logo/logo-footer.webp" alt="footer-logo"></a>
+                <a href="{{ url('/') }}"> 
+                    <img class="w-48" src="storage/image/logo/logo-footer.webp" alt="footer-logo">
+                </a>
             </div>
             <div class="info flex flex-col gap-5">
                 <div class="flex items-center gap-4">
                     <i class="text-[#ec2024] text-lg ri-phone-fill"></i>
-                    <a href="tel:+919560237516" class="text-[#e9c7c7] text-[12px] hover:text-white duration-200 cursor-pointer font-[bold]">+91 95602 37516</a>
+                    <a href="tel:+919560237516" class="text-[#e9c7c7] text-[13px] hover:text-white duration-200 cursor-pointer font-bold">+91 95602 37516</a>
                 </div>
                 <div class="flex items-center gap-4">
                     <i class="text-[#ec2024] text-lg ri-phone-fill"></i>
-                    <a href="tel:+919015961654" class="text-[#e9c7c7] text-[12px] hover:text-white duration-200 cursor-pointer font-[bold]">+91 90159 61654</a>
+                    <a href="tel:+919015961654" class="text-[#e9c7c7] text-[13px] hover:text-white duration-200 cursor-pointer font-bold">+91 90159 61654</a>
                 </div>
                 <div class="flex items-center gap-4">
                     <i class="text-[#ec2024] text-lg ri-mail-fill"></i>
-                    <a href="mailto:contact@doitcreation.com" class="text-[#e9c7c7] text-[12px] hover:text-white duration-200 cursor-pointer font-[bold]">contact@doitcreation.com</a>
+                    <a href="mailto:contact@doitcreation.com" class="text-[#e9c7c7] text-[13px] hover:text-white duration-200 cursor-pointer font-bold">contact@doitcreation.com</a>
                 </div>
             </div>
-            <div class="social flex items-center justify-around">
-                <a target="_blank" href="https://www.facebook.com/doitcreation/"><i class="text-[#e9c7c7] hover:text-white duration-300 text-base cursor-pointer ri-facebook-fill"></i></a>
-                <a target="_blank" href="https://www.instagram.com/doitcreation/"><i class="text-[#e9c7c7] hover:text-white duration-300 text-base cursor-pointer ri-instagram-line"></i></a>
-                <a target="_blank" href="https://www.linkedin.com/company/doitcreation/"><i class="text-[#e9c7c7] hover:text-white duration-300 text-base cursor-pointer ri-linkedin-fill"></i></a>
-                <a target="_blank" href="https://in.pinterest.com/doitcreation/"><i class="text-[#e9c7c7] hover:text-white duration-300 text-base cursor-pointer ri-pinterest-line"></i></a>
+            <div class="social flex items-center gap-4 mt-2">
+                <a target="_blank" href="https://www.facebook.com/doitcreation/"><i class="text-[#e9c7c7] hover:text-white duration-300 text-xl cursor-pointer ri-facebook-fill"></i></a>
+                <a target="_blank" href="https://www.instagram.com/doitcreation/"><i class="text-[#e9c7c7] hover:text-white duration-300 text-xl cursor-pointer ri-instagram-line"></i></a>
+                <a target="_blank" href="https://www.linkedin.com/company/doitcreation/"><i class="text-[#e9c7c7] hover:text-white duration-300 text-xl cursor-pointer ri-linkedin-fill"></i></a>
+                <a target="_blank" href="https://in.pinterest.com/doitcreation/"><i class="text-[#e9c7c7] hover:text-white duration-300 text-xl cursor-pointer ri-pinterest-line"></i></a>
             </div>
         </div>
-        <div class="company flex flex-col gap-6">
-            <h3 class="text-[#ec2024] font-[bold] text-xl">COMPANY</h3>
-            <ul class="flex flex-col gap-2">
-                <li><a class="text-[#e9c7c7] text-[14px] font-[medium] hover:text-white cursor-pointer duration-200" href="{{ url('about-us') }}">About Us</a></li>
-                <li><a class="text-[#e9c7c7] text-[14px] font-[medium] hover:text-white cursor-pointer duration-200" href="{{ url('blog') }}">Blog</a></li>
-                <li><a class="text-[#e9c7c7] text-[14px] font-[medium] hover:text-white cursor-pointer duration-200" href="{{ url('privacy-policy') }}">Privacy Policy</a></li>
-                <li><a class="text-[#e9c7c7] text-[14px] font-[medium] hover:text-white cursor-pointer duration-200" href="{{ url('terms-condition') }}">Terms & Condition</a></li>
-                <li><a class="text-[#e9c7c7] text-[14px] font-[medium] hover:text-white cursor-pointer duration-200" href="{{ url('contact-us') }}">Contact Us</a></li>
-                <li><a class="text-[#e9c7c7] text-[14px] font-[medium] hover:text-white cursor-pointer duration-200" href="{{ url('return-refund') }}">Return & Refund Policy</a></li>
+
+        <div class="company flex flex-col gap-6 w-full">
+            <h3 class="text-[#ec2024] font-bold text-xl uppercase tracking-wide">Company</h3>
+            <ul class="flex flex-col gap-3">
+                <li><a class="text-[#e9c7c7] text-[14px] font-medium hover:text-white cursor-pointer duration-200" href="{{ url('about-us') }}">About Us</a></li>
+                <li><a class="text-[#e9c7c7] text-[14px] font-medium hover:text-white cursor-pointer duration-200" href="{{ url('blog') }}">Blog</a></li>
+                <li><a class="text-[#e9c7c7] text-[14px] font-medium hover:text-white cursor-pointer duration-200" href="{{ url('privacy-policy') }}">Privacy Policy</a></li>
+                <li><a class="text-[#e9c7c7] text-[14px] font-medium hover:text-white cursor-pointer duration-200" href="{{ url('terms-condition') }}">Terms & Condition</a></li>
+                <li><a class="text-[#e9c7c7] text-[14px] font-medium hover:text-white cursor-pointer duration-200" href="{{ url('contact-us') }}">Contact Us</a></li>
+                <li><a class="text-[#e9c7c7] text-[14px] font-medium hover:text-white cursor-pointer duration-200" href="{{ url('return-refund') }}">Return & Refund Policy</a></li>
             </ul>
         </div>
-        <div class="quick-link flex flex-col gap-6">
-              <h3 class="text-[#ec2024] font-[bold] text-xl">QUICK LINKS</h3>
-            <ul class="flex flex-col gap-2">
-                <li><a class="text-[#e9c7c7] text-[14px] font-[medium] hover:text-white cursor-pointer duration-200" href="#">Brand Name Suggestion</a></li>
-                <li><a class="text-[#e9c7c7] text-[14px] font-[medium] hover:text-white cursor-pointer duration-200" href="#">Digital Marketing</a></li>
-                <li><a class="text-[#e9c7c7] text-[14px] font-[medium] hover:text-white cursor-pointer duration-200" href="#">Graphic Design</a></li>
-                <li><a class="text-[#e9c7c7] text-[14px] font-[medium] hover:text-white cursor-pointer duration-200" href="#">Logo Design</a></li>
-                <li><a class="text-[#e9c7c7] text-[14px] font-[medium] hover:text-white cursor-pointer duration-200" href="#">Website Design & Development</a></li>
+
+        <div class="quick-link flex flex-col gap-6 w-full">
+             <h3 class="text-[#ec2024] font-bold text-xl uppercase tracking-wide">Quick Links</h3>
+            <ul class="flex flex-col gap-3">
+                <li><a class="text-[#e9c7c7] text-[14px] font-medium hover:text-white cursor-pointer duration-200" href="#">Brand Name Suggestion</a></li>
+                <li><a class="text-[#e9c7c7] text-[14px] font-medium hover:text-white cursor-pointer duration-200" href="#">Digital Marketing</a></li>
+                <li><a class="text-[#e9c7c7] text-[14px] font-medium hover:text-white cursor-pointer duration-200" href="#">Graphic Design</a></li>
+                <li><a class="text-[#e9c7c7] text-[14px] font-medium hover:text-white cursor-pointer duration-200" href="#">Logo Design</a></li>
+                <li><a class="text-[#e9c7c7] text-[14px] font-medium hover:text-white cursor-pointer duration-200" href="#">Website Design & Development</a></li>
             </ul>
         </div>
-        <div class="map">
-            <iframe class="rounded-xl" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14017.06481277649!2d77.256882!3d28.561768!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce338a7a6659d%3A0x8305c0cae4c7da7!2sDo%20It%20Creation%20-%20Design%20for%20Satisfaction*21!5e0!3m2!1sen!2sin!4v1765541549462!5m2!1sen!2sin" width="280" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
+
+        <div class="map w-full">
+             <iframe class="rounded-xl w-full h-[250px] md:h-[200px]" 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.0665303644483!2d77.37899631508246!3d28.62776898241973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce550a2212921%3A0x6758654c60163351!2sDo%20It%20Creation!5e0!3m2!1sen!2sin!4v1675234567890!5m2!1sen!2sin" 
+                style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+            </div>
     </div>
 
     <div class="p-4 bg-[#818181]">
-        <h3 class="text-center text-sm font-[medium] text-white">© 2025 Do It Creation. All Rights Reserved</h3>
+        <h3 class="text-center text-sm font-medium text-white">© 2025 Do It Creation. All Rights Reserved</h3>
     </div>
 </body>
 
--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -5,83 +5,71 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.7.0/fonts/remixicon.css" rel="stylesheet" />
-    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Responsive Header</title>
 </head>
 
 <body>
 
-    <header class='flex justify-between items-center py-8 px-15 reletive z-1'>
-        <div class=''>
-            <a href="{{ url('/') }}"><img class='w-70 object-cover' src="storage/image/logo/logo.png" alt="" /></a>
+    <header class='flex justify-between items-center py-4 px-4 md:px-8 xl:px-16 relative z-50 w-full'>
+        
+        <div>
+            <a href="{{ url('/') }}">
+                <img class='w-40 md:w-52 xl:w-72 object-cover' src="storage/image/logo/logo.png" alt="Logo" />
+            </a>
         </div>
-        <div class=' backdrop-blur-xs bg-[#ffffff2d] rounded-3xl border-2 border-[#2e2b2845] hidden xl:block'>
-            <div class="flex items-center gap-6 py-1 px-2 rounded-3xl text-black">
-                <li class='list-none font-bold text-sm text-[#2e2b28] hover:text-[#e5212a] duration-200 pl-4'><a href="{{ url('/') }}">Home</a></li>
-                <li class='list-none font-bold text-sm text-[#2e2b28] hover:text-[#e5212a] duration-200'><a href="{{ url('/about-us') }}">About Us</a></li>
-                <li class='list-none font-bold text-sm text-[#2e2b28] hover:text-[#e5212a] duration-200'><a href="{{ url('/services') }}">Services +</a></li>
-                <li class='list-none font-bold text-sm text-[#2e2b28] hover:text-[#e5212a] duration-200'><a href="{{ url('/portfolio') }}">Portfolio</a></li>
-                <li class='list-none font-bold text-sm text-[#2e2b28] hover:text-[#e5212a] duration-200'><a href="{{ url('/digital-product') }}">Digital Product</a></li>
-                <li class='list-none font-bold text-sm text-[#2e2b28] hover:text-[#e5212a] duration-200'><a href="{{ url('/blog') }}">Blog</a></li>
-                <li class='list-none font-bold text-sm text-[#2e2b28] hover:text-[#e5212a] duration-200'><a href="{{ url('/contact-us') }}">Contact Us</a></li>
-                <li class="list-none font-medium text-sm bg-[#e5212a] py-2 px-3 rounded-3xl text-white"><a href="#">Login / signup</a></li>
+
+        <div class='backdrop-blur-md bg-[#ffffff2d] rounded-3xl border-2 border-[#2e2b2845] hidden xl:block'>
+            <div class="flex items-center gap-6 py-2 px-4 rounded-3xl text-black">
+                <ul class="flex items-center gap-6 m-0 p-0">
+                    <li class='list-none font-bold text-sm text-[#2e2b28] hover:text-[#e5212a] duration-200'><a href="{{ url('/') }}">Home</a></li>
+                    <li class='list-none font-bold text-sm text-[#2e2b28] hover:text-[#e5212a] duration-200'><a href="{{ url('/about-us') }}">About Us</a></li>
+                    <li class='list-none font-bold text-sm text-[#2e2b28] hover:text-[#e5212a] duration-200'><a href="{{ url('/services') }}">Services +</a></li>
+                    <li class='list-none font-bold text-sm text-[#2e2b28] hover:text-[#e5212a] duration-200'><a href="{{ url('/portfolio') }}">Portfolio</a></li>
+                    <li class='list-none font-bold text-sm text-[#2e2b28] hover:text-[#e5212a] duration-200'><a href="{{ url('/digital-product') }}">Digital Product</a></li>
+                    <li class='list-none font-bold text-sm text-[#2e2b28] hover:text-[#e5212a] duration-200'><a href="{{ url('/blog') }}">Blog</a></li>
+                    <li class='list-none font-bold text-sm text-[#2e2b28] hover:text-[#e5212a] duration-200'><a href="{{ url('/contact-us') }}">Contact Us</a></li>
+                    <li class="list-none font-medium text-sm bg-[#e5212a] py-2 px-3 rounded-3xl text-white hover:bg-red-700 duration-200"><a href="#">Login / signup</a></li>
+                </ul>
             </div>
         </div>
-        <div id="menu-btn" class="xl:hidden relative w-10 h-10 flex justify-center items-center cursor-pointer z-50">
 
+        <div id="menu-btn" class="xl:hidden relative w-10 h-10 flex justify-center items-center cursor-pointer z-50">
             <i id="icon-menu" class="ri-menu-line text-2xl absolute transition-all duration-300 ease-in-out opacity-100 rotate-0 scale-100"></i>
             <i id="icon-close" class="ri-close-large-fill text-2xl absolute transition-all duration-300 ease-in-out opacity-0 -rotate-90 scale-0"></i>
-
         </div>
     </header>
 
-    <div id="topbar" class="w-70 rounded-xl bg-[#ffffffd3] scale-0 shadow-lg border border-[#e03d3d1c] origin-top-right absolute top-18 right-18  duration-300 z-1 xl:hidden block">
-
-        <ul class="">
+    <div id="topbar" class="w-64 sm:w-72 rounded-xl bg-[#ffffff] shadow-2xl border border-gray-200 origin-top-right absolute top-20 right-4 transform scale-0 duration-300 z-40 xl:hidden block">
+        <ul class="flex flex-col">
             <a href="{{ url('/') }}">
-                <li class="px-5 py-4 rounded-t-xl text-sm hover:bg-red-200 duration-300 cursor-pointer">Home</li>
+                <li class="px-5 py-3 border-b border-gray-100 rounded-t-xl text-sm font-semibold hover:bg-red-50 hover:text-[#e5212a] duration-300 cursor-pointer">Home</li>
             </a>
             <a href="{{ url('/about') }}">
-                <li class="px-5 py-4 text-sm  hover:bg-red-200 duration-300 cursor-pointer">About Us</li>
+                <li class="px-5 py-3 border-b border-gray-100 text-sm font-semibold hover:bg-red-50 hover:text-[#e5212a] duration-300 cursor-pointer">About Us</li>
             </a>
             <a href="{{ url('/services') }}">
-                <li class="px-5 py-4 text-sm hover:bg-red-200 duration-300 cursor-pointer">Services+</li>
+                <li class="px-5 py-3 border-b border-gray-100 text-sm font-semibold hover:bg-red-50 hover:text-[#e5212a] duration-300 cursor-pointer">Services+</li>
             </a>
             <a href="{{ url('/portfolio') }}">
-                <li class="px-5 py-4 text-sm hover:bg-red-200 duration-300 cursor-pointer">Portfolio</li>
+                <li class="px-5 py-3 border-b border-gray-100 text-sm font-semibold hover:bg-red-50 hover:text-[#e5212a] duration-300 cursor-pointer">Portfolio</li>
             </a>
             <a href="{{ url('/digital-product') }}">
-                <li class="px-5 py-4 text-sm hover:bg-red-200 duration-300 cursor-pointer">Digital Product</li>
+                <li class="px-5 py-3 border-b border-gray-100 text-sm font-semibold hover:bg-red-50 hover:text-[#e5212a] duration-300 cursor-pointer">Digital Product</li>
             </a>
             <a href="{{ url('/blog') }}">
-                <li class="px-5 py-4 text-sm hover:bg-red-200 duration-300 cursor-pointer">Blog</li>
+                <li class="px-5 py-3 border-b border-gray-100 text-sm font-semibold hover:bg-red-50 hover:text-[#e5212a] duration-300 cursor-pointer">Blog</li>
             </a>
             <a href="{{ url('/contact-us') }}">
-                <li class="px-5 py-4 text-sm hover:bg-red-200 duration-300 cursor-pointer">Contact Us</li>
+                <li class="px-5 py-3 border-b border-gray-100 text-sm font-semibold hover:bg-red-50 hover:text-[#e5212a] duration-300 cursor-pointer">Contact Us</li>
             </a>
             <a href="#">
-                <li class="px-5 py-4 text-sm rounded-b-xl hover:bg-red-200 duration-300 cursor-pointer">Login / signup</li>
+                <li class="px-5 py-3 rounded-b-xl text-sm font-bold bg-red-50 text-[#e5212a] hover:bg-[#e5212a] hover:text-white duration-300 cursor-pointer">Login / signup</li>
             </a>
         </ul>
     </div>
+
     <script>
-        // let menu = document.getElementById("menu")
-        // let menu_icon = document.getElementById("menu-icon")
-        // let menu_close_icon = document.getElementById("menu-close-icon")
-        // let topbar = document.getElementById("topbar")
-
-        // menu.addEventListener("click", () => {
-        //     if (topbar.style.top === "0%") {
-        //         topbar.style.top = "-100%"
-        //         menu_icon.classList.remove("ri-close-large-fill")
-        //         menu_icon.classList.add("ri-menu-line")
-        //     } else {
-        //         topbar.style.top = "0%"
-        //         menu_icon.classList.remove("ri-menu-line")
-        //         menu_icon.classList.add("ri-close-large-fill")
-        //     }
-        // })
-
-
         const menuBtn = document.getElementById("menu-btn");
         const iconMenu = document.getElementById("icon-menu");
         const iconClose = document.getElementById("icon-close");
@@ -93,6 +81,7 @@
             isOpen = !isOpen;
 
             if (isOpen) {
+                // Menu Open
                 topbar.classList.remove("scale-0");
                 topbar.classList.add("scale-100");
 
@@ -103,9 +92,25 @@
                 iconClose.classList.add("opacity-100", "rotate-0", "scale-100");
 
             } else {
+                // Menu Close
                 topbar.classList.remove("scale-100");
                 topbar.classList.add("scale-0");
 
+                iconMenu.classList.remove("opacity-0", "rotate-90", "scale-0");
+                iconMenu.classList.add("opacity-100", "rotate-0", "scale-100");
+
+                iconClose.classList.remove("opacity-100", "rotate-0", "scale-100");
+                iconClose.classList.add("opacity-0", "-rotate-90", "scale-0");
+            }
+        });
+
+        // Optional: Close menu when clicking outside
+        document.addEventListener('click', (e) => {
+            if (isOpen && !menuBtn.contains(e.target) && !topbar.contains(e.target)) {
+                isOpen = false;
+                topbar.classList.remove("scale-100");
+                topbar.classList.add("scale-0");
+                
                 iconMenu.classList.remove("opacity-0", "rotate-90", "scale-0");
                 iconMenu.classList.add("opacity-100", "rotate-0", "scale-100");
 
